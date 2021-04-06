ba5fb9b80a831b5f442dbd8336937f9fc925352b
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-04-06 12:15:42 +0300

    last test

--- Взаимодействие/js/main.js
+++ Взаимодействие/js/main.js
@@ -70,11 +70,19 @@
 //     alert("Не знаете? “ECMAScript”!");
 // }
 
-let value = prompt("Напишите число", "");
-if (value > 0) {
-    alert("1");
-} else if (value < 0) {
-    alert("-1");
-} else {
-    alert("0");
-}
+// let value = prompt("Напишите число", "");
+// if (value > 0) {
+//     alert("1");
+// } else if (value < 0) {
+//     alert("-1");
+// } else {
+//     alert("0");
+// }
+
+// let result = (a + b < 4) ? "Мало" : "Много";
+
+let message = (login == 'Сотрудник') ? 'Привет' :
+(login == 'Директор') ? 'Здравствуйте' :
+(login == '') ? 'Нет логина' :
+'';
+alert(message);
