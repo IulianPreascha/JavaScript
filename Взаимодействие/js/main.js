634b3490f0d4a5a5262f2121597a93de7e21c0dd
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-04-21 11:28:00 +0300

    switch case

--- Взаимодействие/js/main.js
+++ Взаимодействие/js/main.js
@@ -189,14 +189,14 @@
 // }
 
 switch (number) {
-    case '0': 
+    case 0: 
         alert('Вы ввели число 0');
         break;
-    case '1': 
+    case 1: 
         alert('Вы ввели число 1');
         break;
-    case '2':
-    case '3': 
+    case 2:
+    case 3: 
         alert('Вы ввели число 2, а может и 3');
         break;
 }
