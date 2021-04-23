747c573cf5c8e7a46ff72b60ffbb9a7e27fb121d
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-04-23 10:53:20 +0300

    bad vs good code

--- Взаимодействие/js/main.js
+++ Взаимодействие/js/main.js
@@ -309,12 +309,43 @@
 //   () => alert("Вы отменили выполнение.")
 // );
 
-function pow(x, n) {
-  if (n < 0) {
-    alert("Отрицательные значения 'n' не поддерживаются");
-    return;
-  }
+// function pow(x, n) {
+//   if (n < 0) {
+//     alert("Отрицательные значения 'n' не поддерживаются");
+//     return;
+//   }
 
+//   let result = 1;
+
+//   for (let i = 0; i < n; i++) {
+//     result *= x;
+//   }
+
+//   return result;
+// }
+
+//! Worst code
+
+// function pow(x,n)
+// {
+//   let result=1;
+//   for(let i=0;i<n;i++) {result*=x;}
+//   return result;
+// }
+
+// let x=prompt("x?",''), n=prompt("n?",'')
+// if (n<=0)
+// {
+//   alert(`Степень ${n} не поддерживается, введите целую степень, большую 0`);
+// }
+// else
+// {
+//   alert(pow(x,n))
+// }
+
+//! Good code
+
+function pow(x, n) {
   let result = 1;
 
   for (let i = 0; i < n; i++) {
@@ -322,4 +353,14 @@ function pow(x, n) {
   }
 
   return result;
+}
+
+let x = prompt("x?", "");
+let n = prompt("n?", "");
+
+if (n < 0) {
+  alert(`Степень ${n} не поддерживается,
+    введите целую степень, большую 0`);
+} else {
+  alert( pow(x, n) );
 }
