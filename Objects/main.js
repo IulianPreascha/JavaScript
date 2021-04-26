40924fdbdf83544737fe4836dfd996c9e516762a
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-04-26 13:28:37 +0300

    lesson 2 objects

--- Objects/main.js
+++ Objects/main.js
@@ -216,3 +216,96 @@
 //         };
 //     }
 // };
+
+//! Copying objects and links
+
+// let user = { name: 'Иван' };
+
+// let admin = user;
+
+// admin.name = 'Петя'; // изменено по ссылке из переменной "admin"
+
+// alert(user.name); // 'Петя', изменения видны по ссылке из переменной "user"
+
+//? Two objects are equal in case if they are the same objects
+
+// let a = {};
+// let b = a; // копирование по ссылке
+
+// alert( a == b ); // true, т.к. обе переменные ссылаются на один и тот же объект
+// alert( a === b ); // true
+
+//! Two empty objects are not the same !
+
+// let a = {};
+// let b = {}; // два независимых объекта
+
+// alert( a == b ); // false
+
+//? We can also copy the objects 
+
+// let user = {
+//     name: "Иван",
+//     age: 30
+// };
+
+// let clone = {}; // новый пустой объект
+
+// // скопируем все свойства user в него
+// for (let key in user) {
+//     clone[key] = user[key];
+// }
+
+// // теперь в переменной clone находится абсолютно независимый клон объекта
+// clone.name = "Пётр"; // изменим в нём данные
+
+// alert( user.name ); // в оригинальном объекте значение свойства `name` осталось прежним – Иван.
+
+//? We also can copy it by using Object.assign
+
+// let user = { name: "Иван" };
+
+// let permissions1 = { canView: true };
+// let permissions2 = { canEdit: true };
+
+// // копируем все свойства из permissions1 и permissions2 в user
+// Object.assign(user, permissions1, permissions2);
+
+// // теперь user = { name: "Иван", canView: true, canEdit: true }
+
+// console.table(user);
+
+//* We can change name form objects 
+
+// let user = { name: "Иван" };
+
+// Object.assign(user, { name: "Пётр" });
+
+// alert(user.name); // теперь user = { name: "Пётр" }
+
+//* We can clone objects by using Object.assign 
+
+// let user = {
+//     name: "Иван",
+//     age: 30
+//   };
+  
+//   let clone = Object.assign({}, user);
+
+//* We must know that Object.assign doesn clone objects from object
+
+let user = {
+    name: "Иван",
+    sizes: {
+      height: 182,
+      width: 50
+    }
+  };
+  
+  let clone = Object.assign({}, user);
+  
+  alert( user.sizes === clone.sizes ); // true, один и тот же объект
+  
+  // user и clone обращаются к одному sizes
+  user.sizes.width++;       // меняем свойство в одном объекте
+  alert(clone.sizes.width); // 51, видим результат в другом объекте
