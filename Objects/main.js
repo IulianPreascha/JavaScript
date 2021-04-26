5cb2645e62d4cfc13f3f463334be4da979ba283d
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-04-26 11:13:14 +0300

    object lesson 1

--- Objects/main.js
+++ Objects/main.js
@@ -50,14 +50,89 @@
 
 //? How to acces key ?
 
-let user = {
-    name: "John",
-    age: 30
+// let user = {
+//     name: "John",
+//     age: 30
+// };
+
+// let key = prompt("Что вы хотите узнать о пользователе?", "name");
+
+// // доступ к свойству через переменную
+// alert( user[key] ); // John (если ввели "name")
+
+//? How to calculate properties ?
+
+// let fruit = prompt("Какой фрукт купить?", "apple");
+
+// let bag = {
+//   [fruit]: 5, // имя свойства будет взято из переменной fruit
+// };
+
+// alert( bag.apple ); // 5, если fruit="apple"
+
+//! Property from variable
+
+// function makeUser(name, age) {
+//     return {
+//         name: name,
+//         age: age,
+//     };
+// }
+
+// let user = makeUser("John", 30);
+// console.table(user);
+
+//* We can also use 
+
+// function makeUser(name, age) {
+//     return {
+//       name, // то же самое, что и name: name
+//       age   // то же самое, что и age: age
+//       // ...
+//     };
+// }
+
+//* We can also use both of them 
+
+// let user = {
+//     name,  // тоже самое, что и name:name
+//     age: 30
+//   };
+
+
+//! Restrictions on property names
+
+// эти имена свойств допустимы
+// let obj = {
+//     for: 1,
+//     let: 2,
+//     return: 3
+//   };
+  
+//   alert( obj.for + obj.let + obj.return );  // 6
+
+//* one more 
+
+// let obj = {
+//     0: "Тест" // то же самое что и "0": "Тест"
+//   };
+  
+//   // обе функции alert выведут одно и то же свойство (число 0 преобразуется в строку "0")
+//   alert( obj["0"] ); // Тест
+//   alert( obj[0] ); // Тест (то же свойство)
+
+//? How to check if we have this variable in the object ?
+
+// let user = {name: 'John', age: 30};
+
+// alert( 'age' in user );
+// alert( 'blabla' in user);
+
+//! we need 'in' to check if variable exist in object
+
+let obj = {
+    test: undefined
 };
-
-let key = prompt("Что вы хотите узнать о пользователе?", "name");
-
-// доступ к свойству через переменную
-alert( user[key] ); // John (если ввели "name")
-
-
+  
+alert( obj.test ); //  выведет undefined, значит свойство не существует?
+alert( "test" in obj ); // true, свойство существует!
