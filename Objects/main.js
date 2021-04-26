e432494887cf72485b7cb90dce998144f6faa197
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-04-26 19:28:29 +0300

    practice js

--- Objects/main.js
+++ Objects/main.js
@@ -294,18 +294,26 @@
 
 //* We must know that Object.assign doesn clone objects from object
 
-let user = {
-    name: "Иван",
-    sizes: {
-      height: 182,
-      width: 50
+// let user = {
+//     name: "Иван",
+//     sizes: {
+//       height: 182,
+//       width: 50
+//     }
+//   };
+  
+//   let clone = Object.assign({}, user);
+  
+//   alert( user.sizes === clone.sizes ); // true, один и тот же объект
+  
+//   // user и clone обращаются к одному sizes
+//   user.sizes.width++;       // меняем свойство в одном объекте
+//   alert(clone.sizes.width); // 51, видим результат в другом объекте
+
+function multiplyNumeric(obj) {
+    for( let key in obj ) {
+        if( typeof obj[key] === 'numeric' ) {
+            obj[key] *= 2;
+        }
     }
-  };
-  
-  let clone = Object.assign({}, user);
-  
-  alert( user.sizes === clone.sizes ); // true, один и тот же объект
-  
-  // user и clone обращаются к одному sizes
-  user.sizes.width++;       // меняем свойство в одном объекте
-  alert(clone.sizes.width); // 51, видим результат в другом объекте
+}
