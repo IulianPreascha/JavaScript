5b613b229389195e15adef904f98f5d55327789e
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-04-29 15:12:23 +0300

    datatypes

---
+++ DataTypes/main.js
@@ -0,0 +1,17 @@
+//! Methods 
+
+// let str = "Привет";
+
+// alert( str.toUpperCase() ); // ПРИВЕТ
+
+// let n = 1.23456;
+
+// alert( n.toFixed(2) ); // 1.23
+
+//TODO Homework 
+
+let str = "Привет";
+
+str.test = 5;
+
+alert(str.test);
